2b4331ddc064c8a7db1e1831669be16911d0d98a
Aleksei Abdulin <alexexiv@gmail.com>
2026-09-14 16:36:08 +0500

    Change version

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let flowEntityVersion = "0.1.4"
+let flowEntityVersion = "0.1.5"
 let flowEntityChecksum = "c97232cb42152baafe837abb38cc3dd3757b46441449615eae3c7f50001ff57c"
 
 let package = Package(
